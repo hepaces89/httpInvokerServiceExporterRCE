427ed5e2fa95dc9d24d81166a252acba8f4321a1
Hubert Pan <hubert.pan3@gmail.com>
2021-07-25 13:36:43 -0400

    Add code to customize default serialization process

--- src/main/java/com/icarusfrog/remote/remoting/Account.java
+++ src/main/java/com/icarusfrog/remote/remoting/Account.java
@@ -1,7 +1,11 @@
 package com.icarusfrog.remote.remoting;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
 import java.io.Serializable;
 
+@Slf4j
 public class Account implements Serializable {
     private String name;
 
@@ -11,5 +15,10 @@ public class Account implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+        log.warn("Code running on deserialization");
+        in.defaultReadObject();
     }
 }
